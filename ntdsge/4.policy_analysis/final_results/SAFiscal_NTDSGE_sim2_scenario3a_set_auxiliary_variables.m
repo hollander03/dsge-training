f01d1d1d03b7a2bdddae70a370f2144e46599b13
Hylton Hollander <hollander03@gmail.com>
2024-11-22 09:44:40 +0200

    cleaning

--- ntdsge/4.policy_analysis/final_results/SAFiscal_NTDSGE_sim2_scenario3a_set_auxiliary_variables.m
+++
@@ -1,7 +0,0 @@
-function y = SAFiscal_NTDSGE_sim2_scenario3a_set_auxiliary_variables(y, x, params)
-%
-% Status : Computes static model for Dynare
-%
-% Warning : this file is generated automatically by Dynare
-%           from model file (.mod)
-
--- ntdsge/4.policy_analysis/final_results/SAFiscal_NTDSGE_sim2_scenario4c_set_auxiliary_variables.m
+++
@@ -1,7 +0,0 @@
-function y = SAFiscal_NTDSGE_sim2_scenario4c_set_auxiliary_variables(y, x, params)
-%
-% Status : Computes static model for Dynare
-%
-% Warning : this file is generated automatically by Dynare
-%           from model file (.mod)
-
